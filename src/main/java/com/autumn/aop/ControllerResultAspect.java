a51d0b460d6a288cdb004720f791642bdbc348b1
autumn <qy814379857@foxmail.com>
2018-11-21 14:51:09 +0800

    切面

--- src/main/java/com/autumn/aop/ControllerResultAspect.java
+++ src/main/java/com/autumn/aop/ControllerResultAspect.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.smart4j.framework.annotation.Controller;
 import org.smart4j.framework.aop.Aspect;
 import org.smart4j.framework.aop.AspectProxy;
+
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * 拦截所有Controller方法
  */
 @Aspect(Controller.class)
-public class ControllerAspect extends AspectProxy {
+public class ControllerResultAspect extends AspectProxy {
     private static final Logger LOGGER = LoggerFactory.getLogger(org.smart4j.framework.aop.ControllerAspect.class);
     private long begin;    //方法开始时间
 
@@ -23,9 +27,11 @@ public class ControllerAspect extends AspectProxy {
      */
     @Override
     public void before(Class<?> cls, Method method, Object[] params) throws Throwable {
-        LOGGER.debug("ControllerAspect---------begin---------");
-        LOGGER.debug(String.format("class: %s",cls.getName()));
-        LOGGER.debug(String.format("method: %s",method.getName()));
+        LOGGER.debug("ControllerResultAspect---------begin---------");
+        LOGGER.debug("请求时间:"+new SimpleDateFormat("yyyy-MM-ss HH;mm:ss").format(new Date()));
+        LOGGER.debug(String.format("类: %s",cls.getName()));
+        LOGGER.debug(String.format("方法: %s",method.getName()));
+        LOGGER.debug(String.format("参数: %s", Arrays.toString(params)));
         begin = System.currentTimeMillis();
     }
 
@@ -37,7 +43,7 @@ public class ControllerAspect extends AspectProxy {
      */
     @Override
     public void after(Class<?> cls, Method method, Object result) throws Throwable {
-        LOGGER.debug(String.format("time: %ds", System.currentTimeMillis()-begin));
-        LOGGER.debug("ControllerAspect---------end---------");
+        LOGGER.debug(String.format("结果: %s", result.toString()));
+        LOGGER.debug("ControllerResultAspect---------end---------");
     }
 }
